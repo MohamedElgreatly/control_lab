5bd849b72b79a15477874d7b65cc95530795b6f6
MohamedElgreatly <mohamed.ahmed.elgreatly@gmail.com>
2022-04-20 15:08:23 +0200

    second edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,44 @@ export class AppComponent {
   stringInput:string="";
   ArrOfNodes:any=[];
   ArrForward:any=[];
-  ArrBack:any=[];
-  ArrGainF:any=[];
-  ArrGainB:any=[];
+  ArrGainF:any=[];  
+  ArrForward_res:any=[]; //this the array to access elements it is 2d array
+  ArrGain_res:any=[];   //this the array to access gain in numbers of every nodes it is 2d array
+
   color="green"
   updateNodes(){
     this.ArrOfNodes=[];
-    for(let i=1;i<=this.nodesNum;i++)
+    for(let i=1;i<this.nodesNum;i++)
     {
       this.ArrOfNodes.push({node:'Node'.concat(i.toString()),num:i});
     }
   }
+
+   calc(){
+     for(let i=0;i<this.nodesNum-1;i++)
+     {
+       let tempF:any=[];
+       let tempG:any=[];
+       let strF:string=this.ArrForward[i];
+       let strG:string=this.ArrGainF[i];
+       let str_sepF:string[]=strF.split(",");
+       let str_sepG:string[]=strG.split(",");
+       for(let j=0;j<str_sepF.length;j++)
+       {
+          tempF[j]=Number(str_sepF[j]);
+          tempG[j]=Number(str_sepG[j]);
+       }
+       this.ArrForward_res.push(tempF);
+       this.ArrGain_res.push(tempG);
+
+
+     }
+     //console.log(this.ArrForward_res);
+     //console.log(this.ArrGain_res);
+
+
+   }
+ 
 
 
 }
