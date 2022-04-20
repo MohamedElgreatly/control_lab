ece7dc12a1904d4b7d298bb04e7e264ff0ec53a9
MohamedElgreatly <mohamed.ahmed.elgreatly@gmail.com>
2022-04-20 10:01:51 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lab';
+  nodesNum:number=0;
+  stringInput:string="";
+  ArrOfNodes:any=[];
+  ArrForward:any=[];
+  ArrBack:any=[];
+  ArrGainF:any=[];
+  ArrGainB:any=[];
+  
+  updateNodes(){
+    this.ArrOfNodes=[];
+    for(let i=1;i<=this.nodesNum;i++)
+    {
+      this.ArrOfNodes.push({node:'Node'.concat(i.toString()),num:i});
+    }
+  }
+  
+
+}
