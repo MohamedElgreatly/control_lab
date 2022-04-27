7a23c048f63c6e8572beb0f5486567ca08c1f747
Mahmoud-Ramzy <anatedo4545@gmail.com>
2022-04-27 20:51:07 +0200

    result display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
   ArrForward_res: any = []; //this the array to access elements it is 2d array
   ArrGain_res: any = [];   //this the array to access gain in numbers of every nodes it is 2d array
   isShown: any = false;
+  isShown2: any = false;
   submit1:any=false;
   color = "green"
   vertices: Vertex[] = [];
@@ -59,6 +60,7 @@ export class AppComponent {
   }
   calc() {
     for (let i = 0; i < this.nodesNum - 1; i++) {
+      this.isShown2 = true;
       let tempF: any = [];
       let tempG: any = [];
       let strF: string = this.ArrForward[i];
@@ -87,8 +89,14 @@ let Delta1=0; //numerator
 let Delta2=0; //denemonator
 let pathGain=[]; //total gain for every path.
 let LoopGain=[]; //total gain for every Loop.
+let deelta=0;
+const app = document.getElementById("Result");
+const p2 = document.createElement("p");
+p2.textContent = "-individual loops: " ;
+app?.appendChild(p2);
 for (let i = 0; i < this.loops.length; i++) {//this.directPaths[i] //Path 1
   LoopGain[i]=1;
+  this.PrintVertix(this.loops[i]);
   for (let j = 0; j < this.loops[i].length; j++) {
       LoopGain[i]*=this.loops[i][j].gain; //edge in path i, j
   }
@@ -96,28 +104,45 @@ for (let i = 0; i < this.loops.length; i++) {//this.directPaths[i] //Path 1
 }
 console.log("Loops"+this.loops.length);
 console.log("Gain"+LoopGain.length);
-
+const p1 = document.createElement("p");
+p1.textContent = "-Value of Delta 1 , …, Delta m, Delta : ";
+app?.appendChild(p1);
+const p3 = document.createElement("p");
+p3.textContent = "-All forward paths are:";
+app?.appendChild(p3);
 for (let i = 0; i < this.directPaths.length; i++) {//this.directPaths[i] //Path 1
     pathGain[i]=1;
     for (let j = 0; j < this.directPaths[i].length; j++) {
         pathGain[i]*=this.directPaths[i][j].gain; //edge in path i, j
     }
-    Delta1+=pathGain[i]*this.NonTouchedPath(this.directPaths[i],this.loops,LoopGain);
+    deelta=this.NonTouchedPath(this.directPaths[i],this.loops,LoopGain);
+    Delta1+=pathGain[i]*deelta;
     console.log("the gain path is:"+pathGain[i]);
+    p1.textContent += deelta+", ";
+  app?.appendChild(p1);
   }
 console.log("Delta 1 :"+Delta1);
 Delta2=this.Delta(this.loops, LoopGain);
 console.log("Delta 2 :"+Delta2);
 let result=Delta1/Delta2;
 console.log("Result is :"+result);
+//////////////////////////////////////////////
+
+p1.textContent += "\"Delta: "+Delta2+"\"";
+app?.appendChild(p1);
+const p = document.createElement("p");
+p.textContent = "-Overall system transfer function: "+result;
+app?.appendChild(p);
 }
 // P[i]*Dilta[i] --> Delta1
-// -------------
+// ------------------------
 //   Delta --> Delta 2 "1-(all loops)+(Non touched loops)""
 Delta(Loops:any, Gain:any){
   let Delta=0;
   let Vert1=[];
   let Vert2=[];
+  let NonTouch2=[];
+  let l=0;
   let Touch=false;
   for (let k = 0; k < Gain.length; k++) {
   Delta+=Gain[k];
@@ -137,6 +162,22 @@ Delta=1-Delta;
       }
       if(!Touch){
         Delta+=(Gain[i]*Gain[j]);
+        NonTouch2[l]=i;
+        NonTouch2[l+1]=j;
+        l+=2;
+//        [L1, L2, L1, L3, L2, L3]
+      }
+      for (let q = 0; q <=l ; q+=2) {
+        if(NonTouch2[q]==NonTouch2[q+2]){
+          let temp=q+1; //L2
+          let temp2=q+3; //L3
+          for (let I = q+2; I <= l; I+=2) {
+            if(NonTouch2[q]==NonTouch2[temp]){
+              if(NonTouch2[q+1]==NonTouch2[temp2]){//q, temp, temp2 :3 non touched loops
+                Delta-=Gain[q]*Gain[temp]*Gain[temp2];}
+                                              }
+                                          }
+                                        }
       }
   }
 }
@@ -150,6 +191,16 @@ GetVertix(Loop:any){
     }//we get vertcies for the current loop.
     return Vert;
 }
+PrintVertix(Loop:any){
+  let Vert=[];
+  const app = document.getElementById("Result");
+  const p3 = document.createElement("p");
+  for (let m = 0; m < Loop.length; m++) {
+      Vert[m]=Loop[m].from.name;
+      p3.textContent += ""+Vert[m];
+    }//we get vertcies for the current loop.
+    app?.appendChild(p3);
+  }
 NonTouchedPath(Current:any,ToBeChecked:any,Gain:any){//Current:current path, TBC:all loops
 let Vert=[]; //vertix of current path
 let Check=[]; //loops than non touched with path
@@ -157,9 +208,15 @@ let NonTouch=[];//index of loops that non touched with current path
 let l=0;
 let Delta=0;
 Vert[0]=1;
+const app = document.getElementById("Result");
+const p3 = document.createElement("p");
+p3.textContent += ""+Vert[0];
 for (let index = 0; index <Current.length; index++) {
   Vert[index+1]=Current[index].to.name;
+  p3.textContent += ""+Current[index].to.name;
 }//we get vertcies for the current path.
+p3.textContent += "\n";
+app?.appendChild(p3);
 
 for (let i = 0; i < ToBeChecked.length; i++) {//number of loops
   Check[i]=0;
